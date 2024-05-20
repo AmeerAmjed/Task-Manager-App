7cd1c9fd3f2b8bddaffb46399fea0626e593d2fd
ameeramjed <pr.ameer98@gmail.com>
2024-05-20 13:24:56 +0300

    add dimens design system

---
+++ lib/ui/utils/dimens.dart
@@ -0,0 +1,16 @@
+mixin class Dimens {
+  static const double zero = .0;
+
+  static const double spacing4 = 4.0;
+  static const double spacing8 = 8.0;
+  static const double spacing16 = 16.0;
+
+  static const double radiusSmall = 4.0;
+  static const double radiusFull = 25.0;
+
+  static const double fontSize10 = 10.0;
+  static const double fontSize14 = 14.0;
+  static const double fontSize16 = 16.0;
+  static const double fontSize18 = 18.0;
+  static const double fontSize20 = 20.0;
+}
