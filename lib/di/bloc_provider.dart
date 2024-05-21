ba93e5402987ec86f7dd4c96e6a151e268288b10
ameeramjed <pr.ameer98@gmail.com>
2024-05-21 19:59:25 +0300

    improve home screen

--- lib/di/bloc_provider.dart
+++ lib/di/bloc_provider.dart
@@ -4,7 +4,7 @@ import 'package:task_manager/ui/views/login_screen/bloc/login_screen_bloc.dart';
 
 void setupBlocProvider() {
   getIt.registerLazySingleton<HomeScreenBloc>(
-    () => HomeScreenBloc(),
+    () => HomeScreenBloc()..add(GetTodosEvent()),
   );
 
   getIt.registerLazySingleton<LoginScreenBloc>(
