0e6ce9ac926ffc8ab6279dfa73fe62366cd3d64f
ameeramjed <pr.ameer98@gmail.com>
2024-06-12 01:08:04 +0300

    use fade transition with change route

--- lib/ui/views/home_screen/home_route.dart
+++ lib/ui/views/home_screen/home_route.dart
@@ -3,13 +3,18 @@ import 'package:go_router/go_router.dart';
 import 'package:task_manager/di/get_it.dart';
 import 'package:task_manager/ui/views/home_screen/bloc/home_screen_bloc.dart';
 import 'package:task_manager/ui/views/home_screen/home_screen.dart';
+import 'package:task_manager/ui/widget/build_page_with_fade_transition.dart';
 
 const _route = "/home";
 
 final homeRoute = GoRoute(
   path: _route,
-  builder: (context, state) => BlocProvider(
-    create: (context) => getIt<HomeScreenBloc>(),
-    child: const HomeScreen(),
+  pageBuilder: (context, state) => buildPageWithFadeTransition<void>(
+    context: context,
+    state: state,
+    child: BlocProvider(
+      create: (context) => getIt<HomeScreenBloc>(),
+      child: const HomeScreen(),
+    ),
   ),
 );
