c82b82d8c67013c339196ce0920aefa4a63f412e
ameeramjed <pr.ameer98@gmail.com>
2024-06-11 21:23:52 +0300

    use toast to show message create todo state

--- lib/ui/views/create_todo/bottom_sheet_create_todo.dart
+++ lib/ui/views/create_todo/bottom_sheet_create_todo.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:task_manager/ui/utils/dimens.dart';
 import 'package:task_manager/ui/utils/input_validation.dart';
 import 'package:task_manager/ui/views/create_todo/bloc/create_todo_bloc.dart';
 import 'package:task_manager/ui/views/create_todo/widget/input_create_todo.dart';
 import 'package:task_manager/ui/widget/bottom_sheet.dart';
 import 'package:task_manager/ui/widget/custom_button.dart';
+import 'package:task_manager/ui/widget/toast.dart';
 import 'package:task_manager/utils/is_form_validated.dart';
 
 void bottomSheetCreateTodo(
@@ -22,43 +22,21 @@ void bottomSheetCreateTodo(
         listener: (context, state) {
           if (state.isCreateTodoSuccess) {
             Navigator.of(context).pop();
-            ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text("Successfully!"),
-                  behavior: SnackBarBehavior.floating,
-                  margin: EdgeInsets.all(20),
-                  shape: StadiumBorder(),
-                  action: SnackBarAction(
-                    label: 'Dismiss',
-                    disabledTextColor: Colors.white,
-                    textColor: Colors.blue,
-                    onPressed: () {
-                      //Do whatever you want
-                    },
-                  ),
-                ),
-              );
+            toast(
+              "Successfully create todo",
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              textColor: Theme.of(context).colorScheme.onPrimary,
+            );
           } else if (state.isCreateTodoFailed) {
             Navigator.of(context).pop();
-            ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                content: Text(state.errorMessage ?? ""),
-                behavior: SnackBarBehavior.floating,
-                margin: const EdgeInsets.all(Dimens.spacing16),
-                  shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(Dimens.radius16),
-                ),
-                action: SnackBarAction(
-                    label: 'Dismiss',
-                    disabledTextColor: Colors.white,
-                    textColor: Colors.blue,
-                  onPressed: () {},
-                ),
-                ),
-              );
-            }
-          },
-          builder: (BuildContext context, state) {
+            toast(
+              state.errorMessage ?? "",
+              backgroundColor: Theme.of(context).colorScheme.error,
+              textColor: Theme.of(context).colorScheme.onError,
+            );
+          }
+        },
+        builder: (BuildContext context, state) {
             final createTodoBloc = context.read<CreateTodoBloc>();
 
             return Column(
