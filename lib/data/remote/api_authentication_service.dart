0c3f484088323c1a87adf9b2afccc86ed3d102d2
ameeramjed <pr.ameer98@gmail.com>
2024-06-19 14:59:18 +0300

    Refactor: login in ApiAuthenticationServiceImpl with use baseService

--- lib/data/remote/api_authentication_service.dart
+++ lib/data/remote/api_authentication_service.dart
@@ -1,7 +1,5 @@
-import 'package:dio/dio.dart';
 import 'package:task_manager/data/remote/response/login_response.dart';
 import 'package:task_manager/data/remote/utils/base_service.dart';
-import 'package:task_manager/utils/handle_error.dart';
 
 abstract class ApiAuthenticationService {
   Future<LoginResponse> login(
@@ -19,36 +17,13 @@ class ApiAuthenticationServiceImpl extends BaseApiService
     required String username,
     required String password,
   }) async {
-
-    try {
-      final response = await client.post(
+    return await tryRequest(
+      client.post(
         '$baseUrl/auth/login',
         data: {"username": username, "password": password},
-      );
-
-      if (response.statusCode == 200) {
-        return LoginResponse.fromJson(response.data);
-      } else {
-        throw BadRequestException;
-      }
-    } catch (e) {
-      if (e is BadRequestException) {
-        throw const BadRequestException(message: "Invalid credentials");
-      }
-      if (e is DioException) {
-        if (e.response != null) {
-          if (e.response?.statusCode == 400) {
-            throw const BadRequestException(message: "Invalid credentials");
-          } else {
-            throw ServerException(message: e.toString());
-          }
-        } else {
-          throw ServerException(message: e.message ?? "");
-        }
-      } else {
-        throw ServerException(message: e.toString());
-      }
-    }
+      ),
+      (body) => LoginResponse.fromJson(body),
+    );
   }
 
   @override
