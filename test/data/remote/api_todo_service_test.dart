130af93f35081f30675496477a47c5ce18b5b50a
ameeramjed <pr.ameer98@gmail.com>
2024-06-22 00:52:27 +0300

    improve test and more case

--- test/data/remote/api_todo_service_test.dart
+++ test/data/remote/api_todo_service_test.dart
@@ -1,236 +1,379 @@
-import 'dart:convert';
-
 import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:task_manager/data/remote/api_todo_service.dart';
 import 'package:task_manager/data/remote/model/create_todo_params.dart';
-import 'package:task_manager/data/remote/response/delete_todo_response.dart';
 import 'package:task_manager/data/remote/response/todos_response.dart';
 import 'package:task_manager/utils/handle_error.dart';
 
 import 'api_todo_service_test.mocks.dart';
 
-@GenerateMocks([Dio, ApiToDoServiceImpl, CreateTodoParams])
+@GenerateMocks([Dio])
 void main() {
   late MockDio client;
-  late MockApiToDoServiceImpl apiService;
-  late MockCreateTodoParams mockCreateTodoParams;
+  late ApiToDoService apiService;
 
   setUp(() {
     client = MockDio();
-    apiService = MockApiToDoServiceImpl();
-    mockCreateTodoParams = MockCreateTodoParams();
+    final fakeInterceptors = Interceptors();
+    when(client.interceptors).thenReturn(fakeInterceptors);
+
+    apiService = ApiToDoServiceImpl(client: client);
   });
 
-  group('constructor', () {
-    test('check apiService in provide as well', () {
-      expect(apiService, isNotNull);
-    });
-  });
+  group('ApiToDoService', () {
+    group('getTodos', () {
+      test('getTodos returns TodosResponse on success', () async {
+        //Arrange
+        final fakeResponse = {
+          "todos": [
+            {
+              "id": 1,
+              "todo": "Do something nice for someone I care about",
+              "completed": true,
+              "userId": 26
+            },
+          ],
+          "total": 1,
+          "skip": 0,
+          "limit": 1
+        };
 
-  group('getTodos', () {
-    test('getTodos returns TodosResponse on success', () async {
-      // give in
-      final fakeResponse = jsonEncode({
-        "todos": [
-          {
-            "id": 1,
-            "todo": "Do something nice for someone I care about",
-            "completed": true,
-            "userId": 26
+        final response = Response(
+          data: fakeResponse,
+          statusCode: 200,
+          requestOptions: RequestOptions(path: '/todos'),
+        );
+
+        //when
+        when(client.get(
+          '/todos',
+          queryParameters: {
+            'skip': 0,
+            'limit': 1,
           },
-        ],
-        "total": 1,
-        "skip": 0,
-        "limit": 1
+        )).thenAnswer((_) async => response);
+
+        // Act
+        final result = await apiService.getTodos(skip: 0, limit: 1);
+
+        // Assert
+        expect(result, isA<TodosResponse>());
       });
 
-      final Map<String, dynamic> decodedJson = jsonDecode(fakeResponse);
-      final todosResponse = TodosResponse.fromJson(decodedJson);
+      test('returns TodosResponse on success', () async {
+        // Arrange
+        final fakeResponse = {
+          "todos": [
+            {
+              "id": 1,
+              "todo": "Do something nice for someone I care about",
+              "completed": true,
+              "userId": 26
+            },
+          ],
+          "total": 1,
+          "skip": 0,
+          "limit": 1
+        };
 
-      //when
-      when(apiService.getTodos(skip: 0, limit: 1))
-          .thenAnswer((_) async => todosResponse);
+        final response = Response(
+          data: fakeResponse,
+          statusCode: 200,
+          requestOptions: RequestOptions(path: '/todos'),
+        );
 
-      // Act
-      final result = await apiService.getTodos(skip: 0, limit: 1);
-
-      // Assert
-      expect(result, equals(todosResponse));
-    });
-
-    test('check type response on success', () async {
-      // give in
-      final fakeResponse = jsonEncode({
-        "todos": [
-          {
-            "id": 1,
-            "todo": "Do something nice for someone I care about",
-            "completed": true,
-            "userId": 26
+        // When
+        when(client.get(
+          '/todos',
+          queryParameters: {
+            'skip': 0,
+            'limit': 1,
           },
-        ],
-        "total": 1,
-        "skip": 0,
-        "limit": 1
+        )).thenAnswer((_) async => response);
+
+        // Act
+        final result = await apiService.getTodos(skip: 0, limit: 1);
+
+        // Assert
+        expect(result, isA<TodosResponse>());
+        expect(result.todos.length, equals(1));
       });
 
-      final Map<String, dynamic> decodedJson = jsonDecode(fakeResponse);
-      final todosResponse = TodosResponse.fromJson(decodedJson);
+      test('returns empty TodosResponse when there are no todos', () async {
+        // Arrange
+        final fakeResponse = {"todos": [], "total": 0, "skip": 0, "limit": 1};
 
-      //when
-      when(apiService.getTodos(skip: 0, limit: 1))
-          .thenAnswer((_) async => todosResponse);
+        final response = Response(
+          data: fakeResponse,
+          statusCode: 200,
+          requestOptions: RequestOptions(path: '/todos'),
+        );
 
-      // Act
-      final result = await apiService.getTodos(skip: 0, limit: 1);
+        // When
+        when(client.get(
+          '/todos',
+          queryParameters: {
+            'skip': 0,
+            'limit': 1,
+          },
+        )).thenAnswer((_) async => response);
+
+        // Act
+        final result = await apiService.getTodos(skip: 0, limit: 1);
+
+        // Assert
+        expect(result.todos, isEmpty);
+      });
+
+      test('returns TodosResponse with many todos', () async {
+        // Arrange
+        final fakeResponse = {
+          "todos": List.generate(
+              100,
+              (index) => {
+                    "id": index,
+                    "todo": "Task $index",
+                    "completed": index % 2 == 0,
+                    "userId": index % 10
+                  }),
+          "total": 100,
+          "skip": 0,
+          "limit": 100
+        };
+
+        final response = Response(
+          data: fakeResponse,
+          statusCode: 200,
+          requestOptions: RequestOptions(path: '/todos'),
+        );
+
+        // When
+        when(client.get(
+          '/todos',
+          queryParameters: {
+            'skip': 0,
+            'limit': 100,
+          },
+        )).thenAnswer((_) async => response);
+
+        // Act
+        final result = await apiService.getTodos(skip: 0, limit: 100);
+
+        // Assert
+        expect(result, isA<TodosResponse>());
+        expect(result.todos.length, equals(100));
+      });
+    });
+
+    group('deleteTodo', () {
+      test(
+          'should return response with isDeleted true when item deletion success',
+          () async {
+        //Arrange
+        const todoId = 1;
+        final fakeResponse = {
+          "id": 1,
+          "todo": "Do something nice for someone I care about",
+          "completed": true,
+          "isDeleted": true,
+          "userId": 26
+        };
+
+        final response = Response(
+          data: fakeResponse,
+          statusCode: 200,
+          requestOptions: RequestOptions(path: '/todos/$todoId'),
+        );
+
+        //when
+        when(client.delete(any)).thenAnswer((_) async => response);
+        // Act
+        final result = await apiService.deleteTodo(todoId: 1);
 
       // Assert
-      expect(result, isA<TodosResponse>());
-    });
-  });
-
-  group('deleteTodo', () {
-    test('should return DeleteTodoResponse when successfully delete a todo',
-        () async {
-      //Arrange
-      const response = DeleteTodoResponse(
-        id: 1,
-        todo: 'Test Todo',
-        completed: true,
-        isDeleted: true,
-        userId: 123,
-      );
-      //When
-      when(apiService.deleteTodo(todoId: 1)).thenAnswer((_) async => response);
-      //Act
-      final result = await apiService.deleteTodo(todoId: 1);
-
-      //Assert
-      expect(result, isA<DeleteTodoResponse>());
-    });
-
-    test(
-        'should return response with isDeleted ture when item deletion successes',
-        () async {
-      //Arrange
-      const response = DeleteTodoResponse(
-        id: 1,
-        todo: 'Test Todo',
-        completed: false,
-        isDeleted: true,
-        userId: 123,
-      );
-
-      //when
-      when(apiService.deleteTodo(todoId: 1)).thenAnswer((_) async => response);
-      // Act
-      final result = await apiService.deleteTodo(todoId: 1);
-
-      // Assert
-      verify(apiService.deleteTodo(todoId: 1)).called(1);
       expect(result.isDeleted, isTrue);
     });
 
-    test('should return response with isDeleted false when item deletion fails',
-        () async {
-      //Arrange
-      const response = DeleteTodoResponse(
-        id: 1,
-        todo: 'Test Todo',
-        completed: false,
-        isDeleted: false,
-        userId: 123,
-      );
+      test(
+          'should return response with isDeleted false when item deletion fails',
+          () async {
+        //Arrange
+        const todoId = 1;
+        final fakeResponse = {
+          "id": 1,
+          "todo": "Do something nice for someone I care about",
+          "completed": true,
+          "isDeleted": false,
+          "userId": 26
+        };
 
-      //when
-      when(apiService.deleteTodo(todoId: 1)).thenAnswer((_) async => response);
+        final response = Response(
+          data: fakeResponse,
+          statusCode: 200,
+          requestOptions: RequestOptions(path: '/todos/$todoId'),
+        );
+
+        //when
+            when(client.delete(any)).thenAnswer((_) async => response);
       // Act
       final result = await apiService.deleteTodo(todoId: 1);
 
       // Assert
-      verify(apiService.deleteTodo(todoId: 1)).called(1);
       expect(result.isDeleted, isFalse);
     });
 
-    test('should throw an exception when error', () async {
+      test('should throw NotFoundException when todo id not exist', () async {
+        //Arrange
+        const todoId = 500;
+
+        final response = Response(
+            statusCode: 404,
+            requestOptions: RequestOptions(path: '/todos/$todoId'),
+            data: {"message": "Todo with id $todoId not found"});
+
+        //when
+        when(client.delete(any)).thenAnswer((_) async => response);
+
+        // Assert
+        expect(
+          () async => await apiService.deleteTodo(todoId: todoId),
+          throwsA(isA<NotFoundException>()),
+        );
+      });
+
+      test('should throw ServerException when server error', () async {
+        //Arrange
+        const todoId = 1;
+
+        final response = Response(
+          statusCode: 500,
+          requestOptions: RequestOptions(path: '/todos/$todoId'),
+        );
+
+        //when
+        when(client.delete(any)).thenAnswer((_) async => response);
+
+        // Assert
+        expect(
+          () async => await apiService.deleteTodo(todoId: todoId),
+          throwsA(isA<ServerException>()),
+        );
+      });
+    });
+
+    group('createTodo', () {
+      test('should return TodosResponse when successfully create a todo',
+          () async {
+        //Arrange
+        final fakeResponse = {
+          "id": 1,
+          "todo": "Do something nice for someone I care about",
+          "completed": true,
+          "userId": 26
+        };
+
+        final response = Response(
+                statusCode: 200,
+                requestOptions: RequestOptions(path: '/todos/add'),
+                data: fakeResponse
+            );
+            //When
+            when(client.post(any, data: anyNamed('data'))).thenAnswer((
+                _) async => response);
+
+            //Act
+            final result = await apiService.createTodo(
+              todo: const CreateTodoParams(
+                userId: 1,
+                title: "Do something nice for someone I care about",
+                isCompleted: true,
+              ),
+            );
+
+        //Assert
+        expect(result, true);
+      });
+
+      test('should throw ServerException when userId not exits', () async {
+        //Arrange
+        final fakeResponse = {
+          "todo": "Do something nice for someone I care about",
+          "completed": true,
+          "userId": 5000
+        };
+        const userInput = CreateTodoParams(
+          userId: 5000,
+          title: "Do something nice for someone I care about",
+          isCompleted: true,
+        );
+        final response = Response(
+          statusCode: 400,
+          requestOptions: RequestOptions(path: '/todos/add'),
+          data: fakeResponse,
+        );
+
+        //When
+        when(client.post(any, data: anyNamed('data')))
+            .thenAnswer((_) async => response);
+
+        // Assert
+        expect(
+          () async => await apiService.createTodo(todo: userInput),
+          throwsA(isA<ServerException>()),
+        );
+      });
+    });
+
+    test('should throw Exception when missing required fields', () async {
       // Arrange
-      when(apiService.deleteTodo(todoId: 1))
-          .thenThrow(Exception('Failed to delete'));
+      final fakeResponse = {"error": "Missing required fields"};
+      const userInput = CreateTodoParams(
+        userId: 1,
+        title: "",
+        isCompleted: true,
+      );
+      final response = Response(
+        statusCode: 400,
+        requestOptions: RequestOptions(path: '/todos/add'),
+        data: fakeResponse,
+      );
 
-      // Act & Assert
+      // When
+      when(client.post(any, data: anyNamed('data')))
+          .thenAnswer((_) async => response);
+
+      // Assert
       expect(
-          () async => await apiService.deleteTodo(todoId: 1), throwsException);
-
-      // Verify
-      verify(apiService.deleteTodo(todoId: 1)).called(1);
+        () async => await apiService.createTodo(todo: userInput),
+        throwsA(isA<Exception>()),
+      );
     });
 
-    test('should throw error when todoId is null', () async {
-      //Arrange
-      const todoId = null;
-      when(apiService.deleteTodo(todoId: todoId))
-          .thenThrow(ArgumentError('todoId cannot be null'));
-
-      // Act & Assert
-      expect(() async => await apiService.deleteTodo(todoId: null),
-          throwsA(isA<ArgumentError>()));
-
-      // Verify
-      verify(apiService.deleteTodo(todoId: null)).called(1);
-    });
-
-    test('should throw error when server error', () async {
-      when(apiService.deleteTodo(todoId: 1))
-          .thenThrow(const ServerException(message: 'Network error'));
-
-      // Act & Assert
-      expect(() async => await apiService.deleteTodo(todoId: 1),
-          throwsA(isA<ServerException>()));
-
-      // Verify
-      verify(apiService.deleteTodo(todoId: 1)).called(1);
-    });
-  });
-
-  group('createTodo', () {
-
-    test('should return ture when successfully create a todo', () async {
-      //When
-      when(apiService.createTodo(todo: mockCreateTodoParams))
-          .thenAnswer((_) async => true);
-      //Act
-      final result = await apiService.createTodo(todo: mockCreateTodoParams);
-
-      //Assert
-      expect(result, true);
-    });
-
-    test('should return false when failed create a todo', () async {
-      //When
-      when(apiService.createTodo(todo: mockCreateTodoParams))
-          .thenAnswer((_) async => false);
-      //Act
-      final result = await apiService.createTodo(todo: mockCreateTodoParams);
-
-      //Assert
-      expect(result, false);
-    });
-
-    test('should throw an exception when error', () async {
+    test('should throw ServerException when server error occurs', () async {
       // Arrange
-      when(apiService.createTodo(todo: mockCreateTodoParams))
-          .thenThrow(Exception('Failed to create todo'));
+      final fakeResponse = {"error": "Internal server error"};
+      const userInput = CreateTodoParams(
+        userId: 1,
+        title: "Do something nice for someone I care about",
+        isCompleted: true,
+      );
+      final response = Response(
+        statusCode: 500,
+        requestOptions: RequestOptions(path: '/todos/add'),
+        data: fakeResponse,
+      );
 
-      // Act & Assert
+      // When
+      when(client.post(any, data: anyNamed('data')))
+          .thenAnswer((_) async => response);
+
+      // Assert
       expect(
-          () async => await apiService.createTodo(todo: mockCreateTodoParams),
-          throwsException);
-
-      // Verify
-      verify(apiService.createTodo(todo: mockCreateTodoParams)).called(1);
+        () async => await apiService.createTodo(todo: userInput),
+        throwsA(isA<ServerException>()),
+      );
     });
 
   });
